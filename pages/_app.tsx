47d78c4ce3e6a4cc6696d57a6116b07a829259b3
Jos Broers <jos@jobrodo.nl>
2021-11-15 23:22:42 +0100

    #23: Combined GTM-code (#27)
    * #23: Combined GTM-code
    
    * #23: Fixed indentations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,11 @@
 import "@styles/main.scss"
 import Layout from "@components/Layout"
 import type { AppProps } from "next/app"
-import Script from "next/script"
-import GTM_ID from "@lib/gtm"
-import dynamic from "next/dynamic"
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-	const main = (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	)
-
-	if (!GTM_ID) {
-		return main
-	}
-
-	return (
-		<>
-			<Script id="GoogleTagManager" strategy="afterInteractive">
-				{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-				})(window,document,'script','dataLayer', '${GTM_ID}');`}
-			</Script>
-			{main}
-		</>
-	)
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+	<Layout>
+		<Component {...pageProps} />
+	</Layout>
+)
 
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document"
-import GTM_ID from "@lib/gtm"
+import options from "@data/seo.json"
 
 const Document = () => {
 	return (
-		<Html lang="en">
-			<Head>
-				{GTM_ID ? (
-					<>
-						<link rel="dns-prefetch" href="https://www.googletagmanager.com/" crossOrigin="" />
-						<link rel="dns-prefetch" href="https://www.google-analytics.com/" crossOrigin="" />
-					</>
-				) : undefined}
-			</Head>
+		<Html lang={options.inLanguage}>
+			<Head />
 			<body>
-				{GTM_ID ? (
-					<noscript>
-						<iframe
-							src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-							height="0"
-							width="0"
-							title="Google Tag Manager"
-							style={{ display: "none", visibility: "hidden" }}
-						/>
-					</noscript>
-				) : undefined}
 				<Main />
 				<NextScript />
 			</body>
