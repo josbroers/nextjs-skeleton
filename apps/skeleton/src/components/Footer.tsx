28343fddfa3351744b914ea5aa22ef433ee5cff4
Jos Broers <95633444+JosBroers@users.noreply.github.com>
2022-01-03 16:40:18 +0100

    Add Next.js and Vercel icons to Footer (#75)

--- apps/skeleton/src/components/Footer.tsx
+++ apps/skeleton/src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import styles from '@styles/components/footer.module.scss'
 import Github from "@public/icons/github.svg"
 import Npm from "@public/icons/npm.svg"
+import Vercel from "@public/icons/vercel.svg"
+import Nextjs from "@public/icons/nextjs.svg"
 import {ExternalLink} from 'ui'
 
 /**
@@ -13,7 +15,7 @@ const Footer = () => (
 			href="/github"
 			target="_blank"
 			rel="noreferrer"
-			title="View repository on Github"
+			title="GitHub"
 			className={styles.footer__link}
 		>
 			<Github width={30} height={30}/>
@@ -22,10 +24,28 @@ const Footer = () => (
 			href="/npm"
 			target="_blank"
 			rel="noreferrer"
-			title="View package on NPM"
+			title="NPM"
 			className={styles.footer__link}
 		>
 			<Npm width={30} height={30}/>
+		</ExternalLink>
+		<ExternalLink
+			href="https://nextjs.org/"
+			target="_blank"
+			rel="noreferrer"
+			title="Next.js"
+			className={styles.footer__link}
+		>
+			<Nextjs width={30} height={30}/>
+		</ExternalLink>
+		<ExternalLink
+			href="https://vercel.com"
+			target="_blank"
+			rel="noreferrer"
+			title="Vercel"
+			className={styles.footer__link}
+		>
+			<Vercel width={30} height={30}/>
 		</ExternalLink>
 	</footer>
 )
