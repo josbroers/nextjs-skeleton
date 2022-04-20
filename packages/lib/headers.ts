17b5f08bb3e3de275c2113ea04ad8ca0993ce49d
Jos Broers <jos.broers@indicia.nl>
2022-04-20 20:48:20 +0200

    NEXT-7: Update CSP and packages

--- packages/lib/headers.ts
+++ packages/lib/headers.ts
@@ -8,11 +8,10 @@ const ContentSecurityPolicy = `
     style-src 'self' 'unsafe-inline' *.googleapis.com;
     img-src * blob: data:;
     media-src 'none';
-    connect-src 'self' vitals.vercel-insights.com;
+    connect-src 'self' vitals.vercel-insights.com ws: wss:;
     font-src 'self' *.gstatic.com;
     object-src 'none';
     base-uri 'none';
-    require-trusted-types-for 'script';
   `
 
 /**
