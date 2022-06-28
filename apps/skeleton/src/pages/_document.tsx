49e282ecb755ba04a8d6c68a14abb5438e9ec060
Jos Broers <jos@jobrodo.nl>
2022-06-29 00:09:27 +0200

    Change font-display property

--- apps/skeleton/src/pages/_document.tsx
+++ apps/skeleton/src/pages/_document.tsx
@@ -10,7 +10,7 @@ const Document = () => {
 	return (
 		<Html lang={seo.inLanguage}>
 			<Head>
-				<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
+				<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=optional" rel="stylesheet"/>
 			</Head>
 			<body>
 			<Main/>
