a25ab5e1f0a6b7c6b055688d31625dd975b97e81
Jos Broers <jos@jobrodo.nl>
2022-10-03 23:26:08 +0200

    Update packages

--- apps/skeleton/next.config.js
+++ apps/skeleton/next.config.js
@@ -10,35 +10,27 @@ const nextConfig = {
 	reactStrictMode: true,
 	trailingSlash: true,
 	images: {
-		formats: ["image/avif", "image/webp"],
+		formats: ["image/avif", "image/webp"]
 	},
 	sassOptions: {
 		prependData: `
       @import "../../packages/scss/breakpoints";
 			@import "../../packages/scss/line-clamp";
 			@import "../../packages/scss/unit";
-    `,
+    `
 	},
 	eslint: {
-		dirs: ["src"],
+		dirs: ["src"]
 	},
 	compiler: {
-		removeConsole: process.env.NODE_ENV === "production" ? {exclude: ["error"]} : false,
+		removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false
 	},
 	output: "standalone",
-	webpack: (config, {dev}) => {
-		if (!dev) {
-			Object.assign(config.resolve.alias, {
-				react: "preact/compat",
-				"react-dom/test-utils": "preact/test-utils",
-				"react-dom": "preact/compat",
-			});
-		}
-
+	webpack: (config) => {
 		config.module.rules.push({
 			test: /\.svg$/i,
 			issuer: /\.[jt]sx?$/,
-			use: ["@svgr/webpack"],
+			use: ["@svgr/webpack"]
 		});
 
 		return config;
@@ -48,20 +40,20 @@ const nextConfig = {
 			{
 				source: "/home",
 				destination: "/",
-				permanent: true,
+				permanent: true
 			},
 			{
 				source: "/github",
 				destination: "https://github.com/jos-broers/nextjs-skeleton",
-				permanent: true,
+				permanent: true
 			},
 			{
 				source: "/npm",
 				destination: "https://www.npmjs.com/package/create-nextjs-skeleton",
-				permanent: true,
-			},
+				permanent: true
+			}
 		];
-	},
+	}
 };
 
 module.exports = withTM(nextConfig);
--- apps/skeleton/src/pages/index.tsx
+++ apps/skeleton/src/pages/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import ExternalLink from 'ui/link'
-import Code from 'ui/code'
-import Container from 'ui/container'
-import styles from '@scss/pages/home.module.scss'
+import ExternalLink from "ui/link";
+import Code from "ui/code";
+import Container from "ui/container";
+import styles from "@scss/pages/home.module.scss";
 import Head from "next/head";
-import {Meta, Schema} from "ui/seo";
-import seo from "@data/seo.json"
-import {useCurrentUrl} from 'lib/hooks'
+import { Meta, Schema } from "ui/seo";
+import seo from "@data/seo.json";
+import { useCurrentUrl } from "lib/hooks";
 
 const Home = () => {
-	const title = "Next.js Skeleton: ESLint, Husky, Preact, Prettier, Sass, TypeScript and much more!"
-	const description = "A simple and highly customizable skeleton build with Turborepo and Next.js. Featuring ESLint, Husky, Preact, Prettier, Sass, TypeScript and much more!"
-	const location = useCurrentUrl()
+	const title = "Next.js Skeleton: ESLint, Husky, Prettier, Sass, TypeScript and much more!";
+	const description = "A simple and highly customizable skeleton build with Turborepo and Next.js. Featuring ESLint, Husky, Prettier, Sass, TypeScript and much more!";
+	const location = useCurrentUrl();
 
 	return (
 		<>
@@ -38,13 +38,13 @@ const Home = () => {
 					href={location.href}
 				/>
 			</Head>
-			<Container size={'small'}>
+			<Container size={"small"}>
 				<h1 className={styles.title}>
-					Welcome to the{' '}
+					Welcome to the{" "}
 					<ExternalLink
 						href="/github"
 						title="Next.js starter kit on GitHub"
-						className={styles['cta-link']}
+						className={styles["cta-link"]}
 						target="_blank"
 						rel="noreferrer"
 					>
@@ -52,11 +52,11 @@ const Home = () => {
 					</ExternalLink>
 				</h1>
 				<p className={styles.description}>
-					Get started by editing <Code content="src/pages/index.tsx"/> in the skeleton app.
+					Get started by editing <Code content="src/pages/index.tsx" /> in the skeleton app.
 				</p>
 			</Container>
 		</>
-	)
-}
+	);
+};
 
-export default Home
+export default Home;
